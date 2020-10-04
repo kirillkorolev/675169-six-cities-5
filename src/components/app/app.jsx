1bf4b2b7a4d34e8c986ccdb6211148a6580215bf
Кирилл <kkirillcore@gmail.com>
2020-10-04 20:05:04 +0300

    выполняет домашнее задание по лчиному проекту по второму модулю

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainPage from "../main-page/main-page";
+import AuthScreen from "../auth-screen/auth-screen";
+import FavoritesScreen from "../favorites-screen/favorites-screen";
+import OfferScreen from "../offer-screen/offer-screen";
 
 const App = (props) => {
 
   const {placesAmmount} = props;
 
   return (
-    <MainPage placesAmmount={placesAmmount} />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <MainPage placesAmmount={placesAmmount} />
+        </Route>
+        <Route path="/login" exact>
+          <AuthScreen />
+        </Route>
+        <Route path="/favorites" exact>
+          <FavoritesScreen />
+        </Route>
+        <Route path="/offer/:id?" exact component={OfferScreen}/>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
